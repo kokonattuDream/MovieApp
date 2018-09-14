11680bc8bc9cee50cd2cc99aceccbdcdff41f25e
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-09-13 20:01:46 -0400

    Api service to get movies from api

---
+++ MovieApp/Services/ApiServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MovieApp.Models;
+using Newtonsoft.Json;
+
+namespace MovieApp.Services
+{
+    public class ApiServices
+    {
+        string nowPlayingMoviesUrl = "http://colosseum.somee.com/api/NowPlayingMovies";
+
+        public async Task<List<NowPlayingMovie>> GetNowPlayingMovies()
+        {
+            var client = new HttpClient();
+
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, nowPlayingMoviesUrl);
+
+            requestMessage.Headers.Add("ApiKey", "e0d9b4e1-0310-4f03-978b-3728d1c77b42");
+
+            var responseMessage = await client.SendAsync(requestMessage);
+            var movieResponse = await responseMessage.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<List<NowPlayingMovie>>(movieResponse);
+        }
+    }
+}
